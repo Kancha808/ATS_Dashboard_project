32f8ca362b32a48369928387dc7858458b21b13c
Kanchan Wadode <wadodekanchan@gmail.com>
2026-05-21 15:55:31 +0530

    Temporary App test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import "./App.css";
-import { BrowserRouter } from "react-router-dom"; // Make sure to import this if you plan to use routing
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <h1>ATS Dashboard</h1>
-      </div>
-    </BrowserRouter>
+    <Login />
   );
 }
 
