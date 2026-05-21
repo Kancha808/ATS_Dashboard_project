921769c90c3bfc54abccdcb2a87eb4c8370f15d3
Kanchan Wadode <wadodekanchan@gmail.com>
2026-05-21 16:04:33 +0530

    Temporary App test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Jobs from "./pages/Jobs";
+import Candidates from "./pages/Candidates";
+import CandidateDetails from "./pages/CandidateDetails";
+
+import "./App.css";
 
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/candidates" element={<Candidates />} />
+        <Route
+          path="/candidate-details"
+          element={<CandidateDetails />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
